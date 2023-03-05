a0d1a8077f21c180d83d0d4aade99ab66ccb7758
henrik oliveira <henrik.lopesoliveira@hotmail.com>
2023-03-05 00:05:56 -0300

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 yarn rollup
 cp package.json release/package.json
 cp README.MD release/README.MD
-yes | cp -rf dist/ release/dist/
+yes | cp -rf build/ release/build/
 cd release
 git add .
 git commit -m 'update'
